3ed3175777a989029237dcc5f9e0b1913c78c216
JErenci <joan.erencia@live.de>
2016-04-14 13:55:13 +0200

    Async workers

--- SimpleSmartpointers/SimpleFuture/SimpleFuture.cpp
+++ SimpleSmartpointers/SimpleFuture/SimpleFuture.cpp
@@ -8,6 +8,18 @@
 #include <iostream>
 using namespace std;
 
+
+long sum = 0;
+
+void sumWorker()
+{
+  for ( int i = 0; i < 1000000; i++ )
+  {
+    sum++;
+  }
+}
+
+
 int doWork( char c, int ms )
 {
   for ( int i = 0; i < 10; i++ )
@@ -31,13 +43,22 @@ int func2()
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-  cout << "App startet..." << endl;
+  //cout << "App startet..." << endl;
 
-  future<int> result1( async( func1 ) );
+  //future<int> result1( async( func1 ) );
 
-  int result = func2() + result1.get();
+  //int result = func2() + result1.get();
 
-  cout << "App finished ..." << endl;
+  //cout << "App finished ..." << endl;
+
+  future<void> sum1( async( sumWorker ) );
+  future<void> sum2( async( sumWorker ) );
+  future<void> sum3( async( sumWorker ) );
+
+  sum1.get(); sum2.get(); sum3.get();
+  cout << endl << "sum: " << sum << endl;
+
+  cout << endl << "App finished: " << endl;
 
   cin.get();
 
